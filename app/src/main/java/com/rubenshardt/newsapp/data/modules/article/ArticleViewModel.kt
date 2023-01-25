269c0cd89e4858cd89c4ea544a5fa62067938f9d
Rubens Hardt Junior <rubenshardtster@gmail.com>
2023-01-25 16:22:18 -0300

    Feat: add article view model

---
+++ app/src/main/java/com/rubenshardt/newsapp/data/modules/article/ArticleViewModel.kt
@@ -0,0 +1,30 @@
+package com.rubenshardt.newsapp.data.modules.article
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.rubenshardt.newsapp.data.repositories.ArticlesRepository
+import com.rubenshardt.newsapp.models.Article
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ArticleViewModel @Inject constructor(
+    private val articlesRepository: ArticlesRepository,
+) : ViewModel() {
+
+    private lateinit var currentArticle: Article
+    private val _article = MutableLiveData<Article>()
+    val article: LiveData<Article> = _article
+
+    fun setup(article: Article) {
+        currentArticle = article
+        _article.postValue(article)
+    }
+
+    fun markAsReadClick() {
+        currentArticle.read = !currentArticle.read
+        articlesRepository.updateArticle(currentArticle)
+        _article.postValue(currentArticle)
+    }
+}
